dc420f58b61fe865976c6d488f7aad0258e6d00c
mperry37 <mperry37@alaska.edu>
2019-09-25 18:16:48 -0800

    docs: adds coding standards for cpp file

--- project3/dp3.cpp
+++ project3/dp3.cpp
@@ -11,11 +11,12 @@
 #include <functional>
 using std::function;
 
-/* didItThrow: Takes a function and bool paramters and checks for thrown exception
-** PRE: ff must be a function threw must be a boolean type
-** POST: Runs funtion ff. If exception is thrown set threw=true and rethrows exception
-** TYPES: No requirements on types.
-*/
+/* didItThrow:Takes a function and bool paramters and checks for thrown exception
+** PRE:ff must be a function threw must be a boolean type
+** POST:Runs funtion ff. If exception is thrown set threw=true and rethrows exception
+** TYPES:None
+** ERROR:None
+*/ 
 void didItThrow(const function<void()> & ff,
                 bool & threw)
 {
@@ -32,7 +33,12 @@ void didItThrow(const function<void()> & ff,
     
 }
 
-
+/*gcd:Recursive function to calcualte the gcd of 2 ints
+**PRE:a and b must be >= 0, also a and b can not both be 0.
+**POST:Returns an int that is the GCD of the two ints passed
+**TYPES:None
+**ERRORS:None
+*/
 int gcd(int a,
         int b)
 {
