da6820eb166e194a84bd4a846a7704b73f82801f
mperry37 <mperry37@alaska.edu>
2019-09-25 18:20:22 -0800

    feat: adds gcd fuction definition

--- project3/dp3.cpp
+++ project3/dp3.cpp
@@ -42,7 +42,13 @@ void didItThrow(const function<void()> & ff,
 int gcd(int a,
         int b)
 {
-    return 42;  // Dummy return
-    // TODO: Write this!!!
+   if (a == 0)
+   {
+       return b;
+   }
+   if(a > b){
+       return gcd(b,a);
+   }
+   return gcd((b%a),a);
 }
 
