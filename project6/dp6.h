62dcfe625c37131c130e16fc27411acf77657d57
mperry37 <mperry37@alaska.edu>
2019-11-01 13:36:00 -0800

    init: adds DP6 header file

---
+++ project6/dp6.h
@@ -0,0 +1,42 @@
+// dp6.h
+// Matt Perry
+// Started : 11/1/2019
+// Finished:
+// For CS311 Project6
+// dp6.h is a header file for Reversing a Linked List
+// As well as Associative Dataset Class Template
+
+
+
+#ifndef FILE_DP6_H_INCLUDED
+#define FILE_DP6_H_INCLUDED
+
+#include "llnode2.h"
+
+////////////////////////////////////////////////////////
+// Exercise A Reversing a Linked List
+// Function template named reverseList
+// PRE:
+// POST:
+// ERROR:
+// TYPE REQUIREMENTS:
+// DISC:
+////////////////////////////////////////////////////////
+template<typename T>
+void reverseList(unique_ptr<LLNode2<T>> & head);
+
+
+
+
+////////////////////////////////////////////////////////
+// Exercise B Associative Dataset Class Template
+// PRE:
+// POST:
+// ERROR:
+// TYPE REQUIREMENTS:
+// DISC:
+////////////////////////////////////////////////////////
+
+
+
+#endif //#ifndef FILE_DP6_H_INCLUDED
