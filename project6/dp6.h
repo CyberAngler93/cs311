e57a876c52df4797b73a0d7cc7f262c316b615ce
mperry37 <mperry37@alaska.edu>
2019-11-01 14:02:34 -0800

    skelton files

--- project6/dp6.h
+++ project6/dp6.h
@@ -22,8 +22,8 @@
 // TYPE REQUIREMENTS:
 // DISC:
 ////////////////////////////////////////////////////////
-template<typename T>
-void reverseList(unique_ptr<LLNode2<T>> & head);
+template<typename ValType>
+void reverseList(unique_ptr<LLNode2<ValType>> & head);
 
 
 
@@ -35,8 +35,63 @@ void reverseList(unique_ptr<LLNode2<T>> & head);
 // ERROR:
 // TYPE REQUIREMENTS:
 // DISC:
+// Invariants: 
 ////////////////////////////////////////////////////////
 
+template<typename KVType>
+class LLMap
+{
+////////////////////////////////////////////////////////
+// Data Memeber
+private:
+    std::unique_ptr<LLNode2<KVType>> _data;
+////////////////////////////////////////////////////////
+// Deleted functions
+public:
+    //Copy Ctor
+    LLMap(const LLMap & other) = delete;
+    //Copy Assigment
+    LLMap & operator =(const LLMap & other) = delete;
+    //Move Ctor
+    LLMap(LLMap && other) = delete;
+    //Move Assignment
+    LLMap & operator =(LLMap && other) = delete;
+
+
+////////////////////////////////////////////////////////
+// Member Functions
+public:
+    //Default Constuctor
+    //
+    LLMap();
+
+    //Destructor
+    ~LLMaop();
+
+    //Size of 
+    int size();
+
+    //Empty check
+    bool empty();
+
+    //Find key in const set & non const
+    const KVType * find(value_type key);
+    KVType * find(value_type key);
+
+    //Inserts a key and value into set as long as no key exsist
+    insert(value_type key, size_type value);
+
+    //Erase
+    //Erases a key and vlaue pair by key
+    erase(value_type key);
+
+    template<typename T>
+    traverse();
+
+////////////////////////////////////////////////////////
+
+
+};
 
 
 #endif //#ifndef FILE_DP6_H_INCLUDED
